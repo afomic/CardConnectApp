d7826e522511bb3a69593fee75a3260f13be1954
michael afolabi <afomic1@gmail.com>
2020-01-10 13:12:18 +0100

    added test url

--- App.js
+++ App.js
@@ -8,21 +8,30 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
-  StatusBar,
+    View,
+    StatusBar,
+    StyleSheet
 } from 'react-native';
 
 import HomeScreen from "./src/screen/home/index";
+import FlashMessage from "react-native-flash-message/src/FlashMessage";
 
 const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" backgroundColor={"#4c4297"}/>
-      <SafeAreaView>
-        <HomeScreen/>
-      </SafeAreaView>
-    </>
-  );
+    return (
+        <View style={styles.container}>
+            <StatusBar barStyle="light" backgroundColor={"#4c4297"}/>
+            <FlashMessage
+                position="bottom"
+                duration={30000}
+            />
+            <HomeScreen/>
+        </View>
+    );
 };
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
 
 export default App;
--- src/screen/home/index.js
+++ src/screen/home/index.js
@@ -3,6 +3,8 @@ import {View, StyleSheet, Text} from "react-native"
 import {CreditCardInput} from "react-native-credit-card-input";
 import {Button} from "../../component/button";
 import Toolbar from "../../component/Toolbar";
+import RNCardConnectReactLibrary from 'react-native-card-connect-react-library';
+import {showMessage} from "react-native-flash-message/src/index";
 
 class HomeScreen extends Component {
     constructor(props) {
@@ -14,6 +16,7 @@ class HomeScreen extends Component {
         };
         this._onChange = this._onChange.bind(this);
         this._generateToken = this._generateToken.bind(this);
+        this.setProgress = this.setProgress.bind(this);
     }
 
     render() {
@@ -43,8 +46,36 @@ class HomeScreen extends Component {
         })
     }
 
+    setProgress(loading) {
+        this.setState({
+            loading
+        })
+    }
+
     async _generateToken() {
-        const {card} = this.state;
+        const {number,expiry,cvc} = this.state.card.values;
+        try {
+            this.setProgress(true);
+            const cardRequest = {
+                cardNumber:number.split(" ").join(""),
+                cvv:cvc,
+                expiryDate:expiry
+            };
+            RNCardConnectReactLibrary.setupConsumerApiEndpoint("https://fts.cardconnect.com:443/cardsecure/cs");
+            const response = await RNCardConnectReactLibrary.getCardToken(cardRequest);
+            showMessage({
+                message: "Token successfully generated",
+                description: "Token: "+response,
+                type: "success",
+            });
+            this.setProgress(false);
+        } catch (error) {
+            this.setProgress(false);
+            showMessage({
+                message:"Error: "+error,
+                type: "error",
+            });
+        }
     }
 
 }
